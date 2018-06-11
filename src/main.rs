b85a4be1fb820dd03271eebe96e6813a4d9aaad7
cipepser <respepic@gmail.com>
2018-06-11 21:01:05 +0900

    change structure of directories

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+extern crate pnet;
+
+use pnet::datalink::{self, NetworkInterface};
+use pnet::datalink::Channel::Ethernet;
+use std::env;
+
+fn main() {
+    let interface_name = env::args().nth(1).unwrap();
+    let interface_names_match = |iface: &NetworkInterface| iface.name == interface_name;
+
+    let f = |x| println(interface_name);
+
+    let interfaces = datalink::interfaces();
+    let interface = interfaces.into_iter()
+        .filter(interface_names_match)
+        .next()
+        .unwrap();
+
+    let (mut tx, mut rx) = match datalink::channel(&interface, Default::default()) {
+        Ok(Ethernet(tx, rx)) => (tx, rx),
+        Ok(_) => panic!("Unhandled channel type"),
+        Err(e) => panic!("An error occurred when creating the datalink channel: {}", e)
+    };
+
+    loop {
+        match rx.next() {
+            Ok(packet) => {
+                println!("aaaa")
+            }
+            Err(e) => {
+                panic!("An error occurred while reading: {}", e);
+            }
+        }
+    }
+}
